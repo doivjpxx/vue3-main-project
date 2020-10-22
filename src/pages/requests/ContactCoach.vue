e5b380a5c0aee22e802f0b6698243867b6ee8b54
Huy Phong <phong.bnh@globalfactories.com>
2020-10-22 12:57:24 +0700

    add ContactForm

--- src/pages/requests/ContactCoach.vue
+++ src/pages/requests/ContactCoach.vue
@@ -1,13 +1,87 @@
 <template>
-  <h1>Contact Coach work!</h1>
+  <form @submit.prevent="submitForm">
+    <div class="form-control">
+      <label for="email">Your E-mail</label>
+      <input type="email" id="email" v-model.trim="email"/>
+    </div>
+    <div class="form-control">
+      <label for="message">Message</label>
+      <input type="text" id="message" v-model.trim="message"/>
+    </div>
+    <p class="errors" v-if="!formIsValid">Please enter form!</p>
+    <div class="actions">
+      <base-button>Send Message</base-button>
+    </div>
+  </form>
 </template>
 
 <script>
   export default {
-    name: 'ContactCoach'
+    name: 'ContactCoach',
+    data() {
+      return {
+        email: '',
+        message: '',
+        formIsValid: true,
+      }
+    },
+    methods: {
+      validateForm() {
+
+      },
+      submitForm() {
+        this.formIsValid = true;
+        if (this.email === '' || this.email.includes('@') || this.message === '') {
+          return this.formIsValid = false;
+        }
+
+        // TODO: implement submit form
+
+      }
+    }
   };
 </script>
 
 <style scoped>
+  form {
+    margin: 1rem;
+    border: 1px solid #ccc;
+    border-radius: 12px;
+    padding: 1rem;
+  }
 
+  .form-control {
+    margin: 0.5rem 0;
+  }
+
+  label {
+    font-weight: bold;
+    margin-bottom: 0.5rem;
+    display: block;
+  }
+
+  input,
+  textarea {
+    display: block;
+    width: 100%;
+    font: inherit;
+    border: 1px solid #ccc;
+    padding: 0.15rem;
+  }
+
+  input:focus,
+  textarea:focus {
+    border-color: #3d008d;
+    background-color: #faf6ff;
+    outline: none;
+  }
+
+  .errors {
+    font-weight: bold;
+    color: red;
+  }
+
+  .actions {
+    text-align: center;
+  }
 </style>
